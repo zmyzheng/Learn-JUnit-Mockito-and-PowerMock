480466072712f5975220e456235c660054ebdf1e
Mingyang Zheng <zhengzmy@gmail.com>
2018-10-07 15:41:21 -0700

    part 2: Parameterized JUnit

---
+++ 2 - Parameterized JUnit/src/main/java/com/bharath/maven/calculator/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package com.bharath.maven.calculator.calculator;
+
+public interface Calculator {
+
+	int add(int num1, int num2);
+}
---
+++ 2 - Parameterized JUnit/src/main/java/com/bharath/maven/calculator/calculator/CalculatorImpl.java
@@ -0,0 +1,9 @@
+package com.bharath.maven.calculator.calculator;
+
+public class CalculatorImpl implements Calculator {
+
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+}
